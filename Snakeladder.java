2095c1a6aeaf9ef1045e609760a200ee7bde380b
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-09-20 17:25:43 -0500

    snakeladder

---
+++ Snakeladder.java
@@ -0,0 +1,7 @@
+package Com.bridgelab.Day4.snakeladder;
+
+public class Snakeladder {
+    public static void main(String[] args) {
+     System.out.println("snake and ladder simmulator");
+    }
+}

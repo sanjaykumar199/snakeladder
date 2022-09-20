47c3441e703df98b58532910d14ba67bc7e7d038
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-09-20 18:01:30 -0500

    uc2 player roll dice

--- Snakeladder.java
+++ Snakeladder.java
@@ -1,9 +1,12 @@
 package Com.bridgelab.Day4.snakeladder;
-
+import java.util.*;
 public class Snakeladder {
     public static void main(String[] args) {
+        Random rd = new Random();
      System.out.println("snake and ladder simmulator");
      int SinglePlayer = 0;
      System.out.println("statring position of single player "+ SinglePlayer);
+     int a = rd.nextInt(6)+1;
+     System.out.println("single player roll "+a);
     }
 }

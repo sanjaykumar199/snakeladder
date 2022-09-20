36b3da9d4412bcc771494df5e28ff49a8951ec27
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-09-20 17:37:09 -0500

    uc1 player starting position

--- Snakeladder.java
+++ Snakeladder.java
@@ -3,5 +3,7 @@ package Com.bridgelab.Day4.snakeladder;
 public class Snakeladder {
     public static void main(String[] args) {
      System.out.println("snake and ladder simmulator");
+     int SinglePlayer = 0;
+     System.out.println("statring position of single player "+ SinglePlayer);
     }
 }

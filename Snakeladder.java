c864e3d1e404405dd30dab28a3102594b36ca05c
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-09-21 16:21:31 -0500

    uc3player_noplay_snake_ladder

--- Snakeladder.java
+++ Snakeladder.java
@@ -3,10 +3,27 @@ import java.util.*;
 public class Snakeladder {
     public static void main(String[] args) {
         Random rd = new Random();
-     System.out.println("snake and ladder simmulator");
-     int SinglePlayer = 0;
-     System.out.println("statring position of single player "+ SinglePlayer);
-     int a = rd.nextInt(6)+1;
-     System.out.println("single player roll "+a);
+        System.out.println("snake and ladder simmulator");
+        System.out.println();
+        int SinglePlayer = 0;
+        System.out.println("statring position of single player " + SinglePlayer);
+            int SinglePlayerRolls = rd.nextInt(6) + 1;
+            int a = rd.nextInt(3);
+            System.out.println();
+            System.out.println("The Player roll the die & get a number: " + SinglePlayerRolls);
+            if (a == 0) {
+                System.out.println("No play - stays in the same position");
+                System.out.println("Player current position: " + SinglePlayer);
+            }
+            if (a == 1) {
+                SinglePlayer += SinglePlayerRolls;
+                System.out.println("Get Ladder & player move ahead by number of position: " + SinglePlayerRolls);
+                System.out.println("Player current position: " + SinglePlayer);
+            }
+            if (a == 2) {
+                SinglePlayer -= SinglePlayerRolls;
+                System.out.println("Snake bytes & player move behind by number of position:" + "-" + SinglePlayerRolls);
+                System.out.println("Player current position: " + SinglePlayer);
+            }
+        }
     }
-}
